6ad24dbe55b55171435f5d06414b31c5a111839e
profprogrammeur <contact@profprogrammeur.com>
2021-10-28 23:57:11 +0200

    seeds work

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-Doctor.delete_all
-Patient.delete_all
-Appointment.delete_all
-# City.delete_all
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+City.destroy_all
 
-10.times do
-  user = Patient.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name)
-end
-10.times do
-  user = Doctor.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    specialty: Faker::Educator.course_name,
-    zip_code: Faker::Address.zip_code
-    )
-end
-20.times do
-  Appointment.create!(
-    doctor_id: Faker::Number.between(from:1, to:5),
-    patient_id: Faker::Number.between(from:1, to:5),
-    date: Faker::Time.forward(days: 5,  period: :day, format: :short)
+10.times do |i|
+  City.create!(
+    name: Faker::Address.city,
   )
 end
 
-# 10.times do |i|
-#   City.create!(
-#     name: Faker::Address.city,
-#   )
-# end
-#   puts "Cities created!"
+
+
+10.times do
+  Doctor.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    specialty: Faker::Educator.course_name,
+    zip_code: Faker::Address.zip_code,
+    city: City.all.sample,
+    )
+end
+
+100.times do
+  Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    city: City.all.sample,
+    )
+end
+
+150.times do
+  Appointment.create!(
+    doctor:  Doctor.all.sample,
+    patient: Patient.all.sample,
+    date: Faker::Time.forward(days: 5,  period: :day, format: :short),
+    city: City.all.sample,
+    )
+end
+
+
+  puts "end seeds"
