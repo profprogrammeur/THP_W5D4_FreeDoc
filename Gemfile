051141cc1ec1d566c763907609bf027599b37b8c
profprogrammeur <contact@profprogrammeur.com>
2021-10-28 20:01:10 +0200

    before city

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.4'
-
+gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.6'
+gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+# To reset  =>    rake db:drop db:create db:migrate
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Doctor.delete_all
+Patient.delete_all
+Appointment.delete_all
+# City.delete_all
+
+10.times do
+  user = Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name)
+end
+10.times do
+  user = Doctor.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    specialty: Faker::Educator.course_name,
+    zip_code: Faker::Address.zip_code
+    )
+end
+20.times do
+  Appointment.create!(
+    doctor_id: Faker::Number.between(from:1, to:5),
+    patient_id: Faker::Number.between(from:1, to:5),
+    date: Faker::Time.forward(days: 5,  period: :day, format: :short)
+  )
+end
+
+# 10.times do |i|
+#   City.create!(
+#     name: Faker::Address.city,
+#   )
+# end
+#   puts "Cities created!"
